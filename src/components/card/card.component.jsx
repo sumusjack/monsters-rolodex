ec1d89409b67c0c4b5216f5a2aa31da0227094d8
Jack <jackbrisson1@gmail.com>
2020-12-17 20:01:22 -0800

    update gitignore

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = (props) => (
-    <div className='card-container'>
+    <div className='card-container' >
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h2 key={props.monster.id}> { props.monster.name } </h2>
         <p> { props.monster.email }</p>
+
     </div>
 )
